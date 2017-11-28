8a8b4e76f8c893cdce61e053851dadd18335270b
vinay_kakara <vinaykakara@gmail.com>
2017-11-28 22:46:13 +0530

    Update ui.R

--- Real Time Dahboard/ui.R
+++ Real Time Dahboard/ui.R
@@ -51,20 +51,20 @@ shinyUI(
                     tabItem(tabName = "dashboard",
                             tabsetPanel(
                               
-                              tabPanel(title = "Guide Plate",icon = icon("user"),box(title = "Guide Plate",width = 7, status = "info",solidHeader = TRUE,collapsible = TRUE, bubblesOutput("Bubble1",width = "100%",height = 500)),
-                                       box(title = "Guide Plate ",width = 5, status = "info",solidHeader = TRUE,collapsible = TRUE, tableOutput("Real1")),
-                                       box(title = "Parts Produced",width = 5, status = "info",solidHeader = TRUE,collapsible = TRUE,dataTableOutput("table2"))),
+                              tabPanel(title = "Guide Plate",icon = icon("user"),box(title = "Guide Plate",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, bubblesOutput("Bubble1",width = "100%",height = 500)),
+                                       box(title = "Guide Plate ",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("Real1")),
+                                       box(title = "Parts Produced",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%",tableOutput("table2"))),
                               
-                              tabPanel(title = "PAWL",icon = icon("user"),box(title = "PAWL",width = 7, status = "info",solidHeader = TRUE,collapsible = TRUE, bubblesOutput("Bubble2",width = "100%",height = 500)),
-                                       box(title = "PAWL",width = 5, status = "info",solidHeader = TRUE,collapsible = TRUE, tableOutput("Real2")))
+                              tabPanel(title = "PAWL",icon = icon("user"),box(title = "PAWL",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, bubblesOutput("Bubble2",width = "100%",height = 500)),
+                                       box(title = "PAWL",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("Real2")))
                               
                     )),
                     tabItem(tabName = "guide",
                             tabsetPanel(
                               tabPanel("Guide Plate Data (Hourly)",radioButtons("radio","Type of Barchart:",choices = c("Stack"="1","Dodge"="2"),inline = TRUE),
                                        radioButtons("radio2","Shift data By Hourwise:",choices =c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
-                                       box(title = "Guide Plate - H",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour",width = "100%",height = 500)),
-                                       box(title = "Guide Plate",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE, dataTableOutput("tbl1"))),
+                                       box(title = "Guide Plate - H",width = 10, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour",width = "100%",height = 500)),
+                                       box(title = "Guide Plate",width = 2, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("tbl1"))),
                               tabPanel("Guide Plate Data(Daily)", radioButtons("g1","Type of Barchart:",choices = c("Stack"="1","Dodge"="2","Week Days(Stack)"="3","Week Days(Dodge)"="4"),inline = TRUE),
                                        radioButtons("g2","Choose Shift:", choices = c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
                                        box(title = "Guide Plate - D",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Barchart",width = "100%",height = 500)))
@@ -73,8 +73,8 @@ shinyUI(
                             tabsetPanel(
                               tabPanel("PAWL Data (Hourly)",radioButtons("p1","Type of Barchart:",choices = c("Stack"="1","Dodge"="2"),inline = TRUE),
                                        radioButtons("p2","Shift data By Hourwise:",choices =c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
-                                       box(title = "PAWL (Hourly)",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour1",width = "100%",height = 500)),
-                                       box(title = "PAWL",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE, dataTableOutput("tbl2"))),
+                                       box(title = "PAWL (Hourly)",width = 10, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour1",width = "100%",height = 500)),
+                                       box(title = "PAWL",width = 2, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("tbl2"))),
                               tabPanel("PAWL Data (Daily)",radioButtons("p3","Type of Barchart:",choices = c("Stack"="1","Dodge"="2","Week Days(Stack)"="3","Week Days(Dodge)"="4"),inline = TRUE),
                                        radioButtons("p4","Choose Shift:", choices = c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
                                        box(title = "PAWL - D",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Barchart1",width = "100%",height = 500)))
@@ -83,8 +83,8 @@ shinyUI(
                             tabsetPanel(
                               tabPanel("Guide Plate - Left (Hourly)",radioButtons("l1","Type of Barchart:",choices = c("Stack"="1","Dodge"="2"),inline = TRUE),
                                        radioButtons("l2","Shift data By Hourwise:",choices =c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
-                                       box(title = "Guide Plate-GD1 - H",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour2",width = "100%",height = 500)),
-                                       box(title = "Guide Plate (GD1)",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE, dataTableOutput("tbl3"))),
+                                       box(title = "Guide Plate-GD1 - H",width = 10, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour2",width = "100%",height = 500)),
+                                       box(title = "Guide Plate (GD1)",width = 2, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("tbl3"))),
                               tabPanel("Guide Plate - Left (Daily)",radioButtons("l3","Type of Barchart:",choices = c("Stack"="1","Dodge"="2","Week Days(Stack)"="3","Week Days(Dodge)"="4"),inline = TRUE),
                                        radioButtons("l4","Choose Shift:", choices = c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
                                        box(title = "Guide Plate-GD1 - D",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Left",width = "100%",height = 500)))
@@ -93,8 +93,8 @@ shinyUI(
                             tabsetPanel(
                               tabPanel("Guide Plate - Right (Hourly)",radioButtons("r1","Type of Barchart:",choices = c("Stack"="1","Dodge"="2"),inline = TRUE),
                                        radioButtons("r2","Shift data By Hourwise:",choices =c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
-                                       box(title = "Guide Plate-GD2 - H",width = 8, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour3",width = "100%",height = 500)),
-                                       box(title = "Guide Plate (GD2)",width = 4, status = "info",solidHeader = TRUE,collapsible = TRUE, dataTableOutput("tbl4"))),
+                                       box(title = "Guide Plate-GD2 - H",width = 10, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Hour3",width = "100%",height = 500)),
+                                       box(title = "Guide Plate (GD2)",width = 2, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("tbl4"))),
                               tabPanel("Guide Plate - Right (Daily)",radioButtons("r3","Type of Barchart:",choices = c("Stack"="1","Dodge"="2","Week Days(Stack)"="3","Week Days(Dodge)"="4"),inline = TRUE),
                                        radioButtons("r4","Choose Shift:", choices = c("All Shifts"="1","Shift 1"="2","Shift 2"="3","Shift 3"="4"),inline = TRUE),
                                        box(title = "Guide Plate-GD2 - D",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Right",width = "100%",height = 500)))
@@ -102,13 +102,13 @@ shinyUI(
                     tabItem(tabName = "guide-1",box(title = "Guide Plate (WD1)",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Graph",width = "100%",height = 500)),
                             box(title = "Guide Plate (WD2)",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Graph1",width = "100%",height = 500))),
                     tabItem(tabName = "pawl-1",box(title = "PAWl (WD1)",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE, plotlyOutput("Graph2",width = "100%",height = 500))),
-                    tabItem(tabName = "o",box(title = "Guide Plate (GD1 & GD2)",width = 14, status = "info",solidHeader = TRUE,collapsible = TRUE,dataTableOutput("table")),
-                            box(title = "Guide Plate (Combinations)",width = 14,status = "info",solidHeader = TRUE,collapsible = TRUE, dataTableOutput("table1"))),
+                    tabItem(tabName = "o",box(title = "Guide Plate (GD1 & GD2)",width = 6, status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%",tableOutput("table")),
+                            box(title = "Guide Plate (Combinations)",width = 3,status = "info",solidHeader = TRUE,collapsible = TRUE,style = "font-size: 150%; width: 100%", tableOutput("table1"))),
                     tabItem(tabName = "h",actionButton("generate", "Generate PDF"),uiOutput("pdfview")),
                     tabItem(tabName = "pd",helpText(   a("Click Here for Production Dashboard",     href="https://tiproject.shinyapps.io/PDashboard/")
                     )),
-                    tabItem(tabName = "m",box(title = "Guide Plate (Tolerance Band)",width = 6, status = "info",solidHeader = TRUE,collapsible = TRUE,dataTableOutput("table3")),
-                            box(title = "PAWL (Tolerance Band)",width = 6, status = "info",solidHeader = TRUE,collapsible = TRUE,dataTableOutput("table4"),valueBoxOutput("x")))
+                    tabItem(tabName = "m",box(title = "Guide Plate (Tolerance Band)",width = 4,style = "font-size: 150%; width: 100%", status = "info",solidHeader = TRUE,collapsible = TRUE,tableOutput("table3")),
+                            box(title = "PAWL (Tolerance Band)",width = 4,style = "font-size: 150%; width: 100%", status = "info",solidHeader = TRUE,collapsible = TRUE,tableOutput("table4"),valueBoxOutput("x")))
                     
                   )
                 )
